76232529474d06878d1c9be3d639a75a27281ecf
NarvekarHritika <hritikavn72@gmail.com>
2025-12-22 00:23:43 +0530

    formating files

--- day1/part1.py
+++ day1/part1.py
@@ -1,24 +1,25 @@
 def shift_dail_to_left(dail_position, distance):
     return dail_position - distance
 
+
 def shift_dail_to_right(dail_position, distance):
     return dail_position + distance
 
-dail_position=50
-count_for_dail_at_0=0
+
+dail_position = 50
+count_for_dail_at_0 = 0
 with open("day1/input.txt") as file:
     for input in file:
         input = input.rstrip()
-        distance = int(input[1:]) %100
+        distance = int(input[1:]) % 100
         if input[0] == "L":
             dail_position = shift_dail_to_left(dail_position, distance)
         else:
             dail_position = shift_dail_to_right(dail_position, distance)
-        dail_position=dail_position %100
+        dail_position = dail_position % 100
 
         if dail_position == 0:
-            count_for_dail_at_0 = count_for_dail_at_0 +1
-       
+            count_for_dail_at_0 = count_for_dail_at_0 + 1
+
 print(dail_position)
 print(count_for_dail_at_0)
-
--- day2/part2.py
+++ day2/part2.py
@@ -2,31 +2,34 @@ def is_repeated_half(s: str) -> bool:
     half = len(s) // 2
     return s[:half] == s[half:]
 
+
 def subset_chek(s: str, k: int) -> str:
-    i=0
-    while(i+k < len(s)):
-        j=i+k
-        if s[i:j] == s[j:j+k]:
-            i+=k
+    i = 0
+    while i + k < len(s):
+        j = i + k
+        if s[i:j] == s[j : j + k]:
+            i += k
         else:
             return "no_match"
     return "match"
 
+
 def check_repeated_in_subset(s: str) -> bool:
-    for k in range(1,len(s)):
-        if len(s)%k==0:
-            if subset_chek(s, k)=="match":
-                return True      
+    for k in range(1, len(s)):
+        if len(s) % k == 0:
+            if subset_chek(s, k) == "match":
+                return True
     return False
-    
+
+
 total = 0
 with open("day2/input.txt") as file:
     for line in file:
-        line = line.split(',')
+        line = line.split(",")
         for part in line:
-            [start_no, end_no] = map(int, part.split('-'))
-            for i in range(start_no, end_no+1):
-                s=str(i)
+            [start_no, end_no] = map(int, part.split("-"))
+            for i in range(start_no, end_no + 1):
+                s = str(i)
                 if check_repeated_in_subset(s):
                     total += i
 
